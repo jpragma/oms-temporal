db2919569bdafa18a2b1b6de645a1fbd931bcb70
Isaac Levin <ilevin@jpragma.com>
2021-09-06 15:16:32 -0400

    Remove/comment out unnecessary config from the root gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.5.30"
     id("org.jetbrains.kotlin.kapt") version "1.5.30"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
-    id("io.micronaut.application") version "2.0.3"
+    id("com.github.johnrengelman.shadow") version "7.0.0" apply false
+    id("io.micronaut.application") version "2.0.3" apply false
     id("org.jetbrains.kotlin.plugin.allopen") version "1.5.30"
     kotlin("plugin.serialization") version "1.5.30"
 }
@@ -44,42 +44,29 @@ subprojects {
     }
 }
 
-micronaut {
-    runtime("netty")
-    testRuntime("junit5")
-    processing {
-        incremental(true)
-        annotations("com.jpragma.*")
-    }
-}
-
 dependencies {
-    kapt("io.micronaut.data:micronaut-data-processor")
-    implementation("io.micronaut:micronaut-http-client")
-    implementation("io.micronaut:micronaut-runtime")
-    implementation("io.micronaut.data:micronaut-data-jdbc")
-    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
-    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
-    implementation("javax.annotation:javax.annotation-api")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
-    runtimeOnly("ch.qos.logback:logback-classic")
-    runtimeOnly("org.postgresql:postgresql")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql")
-    testImplementation("org.testcontainers:testcontainers")
-    implementation("io.micronaut:micronaut-validation")
-
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+//    kapt("io.micronaut.data:micronaut-data-processor")
+//    implementation("io.micronaut:micronaut-http-client")
+//    implementation("io.micronaut:micronaut-runtime")
+//    implementation("io.micronaut.data:micronaut-data-jdbc")
+//    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+//    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
+//    implementation("javax.annotation:javax.annotation-api")
+//    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+//    runtimeOnly("ch.qos.logback:logback-classic")
+//    runtimeOnly("org.postgresql:postgresql")
+//    testImplementation("org.testcontainers:junit-jupiter")
+//    testImplementation("org.testcontainers:postgresql")
+//    testImplementation("org.testcontainers:testcontainers")
+//    implementation("io.micronaut:micronaut-validation")
+//
+//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
 
 
 }
 
-
-application {
-    mainClass.set("com.jpragma.ApplicationKt")
-}
-java {
-    sourceCompatibility = JavaVersion.toVersion("11")
-}
+//java {
+//    sourceCompatibility = JavaVersion.toVersion("11")
+//}
